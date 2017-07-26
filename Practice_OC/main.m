6b6c43df308f5a1e533ef99a1b5afcb59612b1d2
JTL1992 <talentjiang@yeah.net>
2017-07-26 14:35:05 +0200

    Initial Commit

---
+++ Practice_OC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Practice_OC
+//
+//  Created by nuonuo-jtl on 17/7/26.
+//  Copyright © 2017年 nuonuo-jtl. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
