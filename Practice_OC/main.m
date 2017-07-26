2a39a90f38c0b987e2f95c83cd0f3fec558aa664
JTL1992 <talentjiang@yeah.net>
2017-07-26 15:49:43 +0200

    commit01 variables

--- Practice_OC/main.m
+++ Practice_OC/main.m
@@ -7,11 +7,28 @@
 //
 
 #import <Foundation/Foundation.h>
+#define LENGTH 10
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        /* variable definition: */
+        int a, b;
+        int c;
+        float f;
+        const int WIDTH = 5;
+        
+        /* actual initialization */
+        a = 10;
+        b = 20;
+        
+        c = a + b;
+        NSLog(@"value of c : %d \n", c);
+        
+        f = 70.0/3.0;
+        NSLog(@"value of f : %f \n", f);
+        
+        NSLog(@"hello, length is %d", LENGTH * WIDTH);
+        
     }
     return 0;
 }
---
+++ Practice_OC/variables.m
@@ -0,0 +1,17 @@
+//
+//  variables.m
+//  Practice_OC
+//
+//  Created by nuonuo-jtl on 17/7/26.
+//  Copyright © 2017年 nuonuo-jtl. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+extern int a, b;
+extern int c;
+extern float f;
+
+
+
+
